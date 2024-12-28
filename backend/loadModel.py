8df5f20e16170fba5d541e57ebe5b72c2caf0da9
061672 <elmehdi.hassani@intelcia.com>
2024-12-28 17:52:29 +0100

    remove spring boot

--- backend/loadModel.py
+++
@@ -1,3 +0,0 @@
-from transformers import DonutProcessor, VisionEncoderDecoderModel
-DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2").save_pretrained("./models/donut-base-finetuned-cord-v2")
-VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2").save_pretrained("./models/donut-base-finetuned-cord-v2")
