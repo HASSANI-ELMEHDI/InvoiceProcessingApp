7c48fea1c825aa101907a11e5641b8ed8e692b37
061672 <elmehdi.hassani@intelcia.com>
2024-12-28 19:11:23 +0100

    add docker compose

--- run.bat
+++ run.bat
@@ -1,10 +1,7 @@
 @echo off
 
 REM Commande Python (depuis le répertoire parent)
-start cmd /k "cd .\backend\model\ && python .\server.py && pause"
-
-REM Commande Maven (dans un sous-répertoire relatif)
-start cmd /k "cd .\backend\services\data-extraction-service\ && mvn spring-boot:run && pause"
+start cmd /k "cd .\backend\ && python .\server.py && pause"
 
 REM Commande NPM (dans un autre sous-répertoire relatif)
-start cmd /k "cd .\frontend-nextjs\ && npm run dev && pause"
+start cmd /k "cd .\frontend\ && npm run dev && pause"
